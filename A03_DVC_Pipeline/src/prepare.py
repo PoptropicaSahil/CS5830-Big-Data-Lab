74316cb4062b418b92e37af6d2b45059cb824002
Sahil <ed19b048@smail.iitm.ac.in>
2024-03-27 09:45:05 +0530

    added logic to get useful files from archive

--- A03_DVC_Pipeline/src/prepare.py
+++ A03_DVC_Pipeline/src/prepare.py
@@ -30,3 +30,54 @@ if __name__ == "__main__":
 
     a03_logger.info(f'input files are {input_files}')
     extract_monthly_aggregates(input_files, output_file)
+
+
+
+
+# LOGIC LOOOOOOOP
+# import concurrent.futures
+# import threading
+# import pandas as pd
+# import time
+
+
+# useful_df = []
+# n_locs = 2
+# lock = threading.Lock()
+
+# # Define the download_urls
+# files_read = 0  # Initialize files_read
+
+# def process_url(url):
+#     global files_read
+
+#     cols_to_read = ['MonthlyAverageRH', 'MonthlyDewpointTemperature', 'MonthlyMeanTemperature',
+#                     'MonthlySeaLevelPressure', 'MonthlyStationPressure', 'MonthlyTotalLiquidPrecipitation',
+#                     'MonthlyTotalSnowfall','MonthlyWetBulb']
+
+#     df = pd.read_csv(url, usecols = cols_to_read)
+#     # monthly_agg_cols = [i for i in df.columns if i.startswith('Monthly')]
+#     # df_agg = df[monthly_agg_cols]
+
+#     # df_agg = df.copy()
+
+#     with lock:
+#         files_read += 1
+#         print(f'Read {files_read} csv', flush=True)
+
+#     if df.isnull().all().all(): # df_agg
+#         print(f'not useful df {url}')
+#     else:
+#         useful_df.append(url)
+#         print(f'***** useful df found for {url}')
+        
+#         with lock:
+#             if len(useful_df) == n_locs:
+#                 print('Found enough URLs, breaking...')
+#                 return False
+            
+#     return True
+
+# with concurrent.futures.ThreadPoolExecutor() as executor:
+#     num_files_to_read = n_locs * 10
+#     executor.map(process_url, download_urls[::-1][:num_files_to_read])
